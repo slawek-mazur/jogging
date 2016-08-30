0aa0b27a78eb052a1c2a2f623b67151715584bef
Slawek Mazur <mazursllawek@gmail.com>
2016-08-30 20:13:06 +0200

    - test list

--- src/main/java/io/stricte/jogging/web/RunController.java
+++ src/main/java/io/stricte/jogging/web/RunController.java
@@ -3,6 +3,7 @@ package io.stricte.jogging.web;
 import io.stricte.jogging.domain.Run;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Collection;
 import java.util.Collections;
 
+import static io.stricte.jogging.config.security.Role.ROLE_USER;
+
 @Controller
 @RequestMapping("/run")
+@Secured(ROLE_USER)
 public class RunController {
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
--- src/test/java/io/stricte/jogging/web/RunControllerIT.java
+++ src/test/java/io/stricte/jogging/web/RunControllerIT.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,13 +48,34 @@ public class RunControllerIT {
     }
 
     @Test
-    public void testSecurityIsOk() throws Exception {
+    public void testRedirectIfNotLoggedIn() throws Exception {
         mockMvc.perform(
             get("/run")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaType.APPLICATION_JSON_UTF8)
         )
-            .andExpect(status().isOk());
+            .andExpect(status().is3xxRedirection());
+    }
 
+    @Test
+    public void testListAsUser() throws Exception {
+        mockMvc.perform(
+            get("/run")
+                .with(user("joe").roles("USER"))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+        )
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testListAsAdmin() throws Exception {
+        mockMvc.perform(
+            get("/run")
+                .with(user("joe").roles("ADMIN"))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+        )
+            .andExpect(status().isOk());
     }
 }
