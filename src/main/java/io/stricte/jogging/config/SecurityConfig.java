8f10c8107e918f45b631de6ed79313a0010e2d22
Slawek Mazur <mazursllawek@gmail.com>
2016-08-30 19:26:15 +0200

    - introduce security roles

--- src/main/java/io/stricte/jogging/config/SecurityConfig.java
+++ src/main/java/io/stricte/jogging/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.stricte.jogging.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,12 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Autowired
+    DefaultWebSecurityExpressionHandler expressionHandler;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -40,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
             .and()
                 .authorizeRequests()
+                    .expressionHandler(expressionHandler)
                     .antMatchers(
                         "/system/**"
                     ).denyAll()
@@ -54,11 +60,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Configuration
-    static class EncoderConfig {
+    static class SecurityAuxiliaryConfig {
 
         @Bean
         PasswordEncoder passwordEncoder() {
             return new StandardPasswordEncoder();
+        }
+
+        @Bean
+        DefaultWebSecurityExpressionHandler expressionHandler() {
+            final DefaultWebSecurityExpressionHandler handler =
+                new DefaultWebSecurityExpressionHandler();
+
+            final RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+            roleHierarchy.setHierarchy("ROLE_MASTER > ROLE_ADMIN and ROLE_ADMIN > ROLE_USER");
+            handler.setRoleHierarchy(roleHierarchy);
+
+            return handler;
         }
     }
 }
