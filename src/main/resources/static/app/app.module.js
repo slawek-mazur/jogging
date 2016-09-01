1029ded6051533b400713da6247ab1a1d8a684a7
Slawek Mazur <mazursllawek@gmail.com>
2016-09-01 20:57:25 +0200

    - some initial setup for client

---
+++ src/main/resources/static/app/app.module.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('jogging', [
+            'ui.bootstrap',
+            'ui.bootstrap.datetimepicker',
+            'ui.router'
+        ])
+        .run(run);
+
+    run.$inject = ['stateHandler'];
+
+    function run(stateHandler) {
+        stateHandler.initialize();
+    }
+})();
