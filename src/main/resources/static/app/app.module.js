7951d875168cc23f1fa2b21123f92a0fb328bbad
Slawek Mazur <mazursllawek@gmail.com>
2016-09-02 20:28:11 +0200

    - ribbon - constant

--- src/main/resources/static/app/app.module.js
+++ src/main/resources/static/app/app.module.js
@@ -3,9 +3,24 @@
 
     angular
         .module('jogging', [
+            'ngStorage',
+            'ngResource',
+            'ngCookies',
+            'ngAria',
+            'ngCacheBuster',
+            'ngFileUpload',
             'ui.bootstrap',
             'ui.bootstrap.datetimepicker',
-            'ui.router'
+            'ui.router',
+            'infinite-scroll',
+            // jhipster-needle-angularjs-add-module JHipster will add new module here
+            'angular-loading-bar'
         ])
-        .run();
+        .run(run);
+
+    run.$inject = ['stateHandler'];
+
+    function run(stateHandler) {
+        stateHandler.initialize();
+    }
 })();
