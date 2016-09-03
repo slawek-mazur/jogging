f77159ef0f3440661eda6bf64add8a8017083bf9
Slawek Mazur <mazursllawek@gmail.com>
2016-09-03 13:46:38 +0200

    - working around round entity

--- src/main/resources/static/app/entities/run/run.state.js
+++ src/main/resources/static/app/entities/run/run.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -9,175 +9,175 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-        .state('run', {
-            parent: 'entity',
-            url: '/run?page&sort&search',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'Runs'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/run/runs.html',
-                    controller: 'RunController',
-                    controllerAs: 'vm'
-                }
-            },
-            params: {
-                page: {
-                    value: '1',
-                    squash: true
+            .state('run', {
+                parent: 'entity',
+                url: '/run?page&sort&search',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'Runs'
                 },
-                sort: {
-                    value: 'id,asc',
-                    squash: true
-                },
-                search: null
-            },
-            resolve: {
-                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
-                    return {
-                        page: PaginationUtil.parsePage($stateParams.page),
-                        sort: $stateParams.sort,
-                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
-                        ascending: PaginationUtil.parseAscending($stateParams.sort),
-                        search: $stateParams.search
-                    };
-                }]
-            }
-        })
-        .state('run-detail', {
-            parent: 'entity',
-            url: '/run/{id}',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'Run'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/run/run-detail.html',
-                    controller: 'RunDetailController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                entity: ['$stateParams', 'Run', function($stateParams, Run) {
-                    return Run.get({id : $stateParams.id}).$promise;
-                }],
-                previousState: ["$state", function ($state) {
-                    var currentStateData = {
-                        name: $state.current.name || 'run',
-                        params: $state.params,
-                        url: $state.href($state.current.name, $state.params)
-                    };
-                    return currentStateData;
-                }]
-            }
-        })
-        .state('run-detail.edit', {
-            parent: 'run-detail',
-            url: '/detail/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/run/run-dialog.html',
-                    controller: 'RunDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Run', function(Run) {
-                            return Run.get({id : $stateParams.id}).$promise;
-                        }]
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/run/runs.html',
+                        controller: 'RunController',
+                        controllerAs: 'vm'
                     }
-                }).result.then(function() {
-                    $state.go('^', {}, { reload: false });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('run.new', {
-            parent: 'run',
-            url: '/new',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/run/run-dialog.html',
-                    controller: 'RunDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: function () {
-                            return {
-                                day: null,
-                                distance: null,
-                                time: null,
-                                id: null
-                            };
+                },
+                params: {
+                    page: {
+                        value: '1',
+                        squash: true
+                    },
+                    sort: {
+                        value: 'id,asc',
+                        squash: true
+                    },
+                    search: null
+                },
+                resolve: {
+                    pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                        return {
+                            page: PaginationUtil.parsePage($stateParams.page),
+                            sort: $stateParams.sort,
+                            predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                            ascending: PaginationUtil.parseAscending($stateParams.sort),
+                            search: $stateParams.search
+                        };
+                    }]
+                }
+            })
+            .state('run-detail', {
+                parent: 'entity',
+                url: '/run/{id}',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'Run'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/run/run-detail.html',
+                        controller: 'RunDetailController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    entity: ['$stateParams', 'Run', function ($stateParams, Run) {
+                        return Run.get({id: $stateParams.id}).$promise;
+                    }],
+                    previousState: ["$state", function ($state) {
+                        return {
+                            name: $state.current.name || 'run',
+                            params: $state.params,
+                            url: $state.href($state.current.name, $state.params)
+                        };
+                    }]
+                }
+            })
+            .state('run-detail.edit', {
+                parent: 'run-detail',
+                url: '/detail/edit',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/run/run-dialog.html',
+                        controller: 'RunDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Run', function (Run) {
+                                return Run.get({id: $stateParams.id}).$promise;
+                            }]
                         }
-                    }
-                }).result.then(function() {
-                    $state.go('run', null, { reload: 'run' });
-                }, function() {
-                    $state.go('run');
-                });
-            }]
-        })
-        .state('run.edit', {
-            parent: 'run',
-            url: '/{id}/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/run/run-dialog.html',
-                    controller: 'RunDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Run', function(Run) {
-                            return Run.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('run', null, { reload: 'run' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('run.delete', {
-            parent: 'run',
-            url: '/{id}/delete',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/run/run-delete-dialog.html',
-                    controller: 'RunDeleteController',
-                    controllerAs: 'vm',
-                    size: 'md',
-                    resolve: {
-                        entity: ['Run', function(Run) {
-                            return Run.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('run', null, { reload: 'run' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        });
+                    }).result.then(function () {
+                        $state.go('^', {}, {reload: false});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('run.new', {
+                parent: 'run',
+                url: '/new',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/run/run-dialog.html',
+                        controller: 'RunDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {
+                                    day: null,
+                                    distance: 0,
+                                    duration: 0,
+                                    averageSpeed: 0.0,
+                                    id: null
+                                };
+                            }
+                        }
+                    }).result.then(function () {
+                        $state.go('run', null, {reload: 'run'});
+                    }, function () {
+                        $state.go('run');
+                    });
+                }]
+            })
+            .state('run.edit', {
+                parent: 'run',
+                url: '/{id}/edit',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/run/run-dialog.html',
+                        controller: 'RunDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Run', function (Run) {
+                                return Run.get({id: $stateParams.id}).$promise;
+                            }]
+                        }
+                    }).result.then(function () {
+                        $state.go('run', null, {reload: 'run'});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('run.delete', {
+                parent: 'run',
+                url: '/{id}/delete',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/run/run-delete-dialog.html',
+                        controller: 'RunDeleteController',
+                        controllerAs: 'vm',
+                        size: 'md',
+                        resolve: {
+                            entity: ['Run', function (Run) {
+                                return Run.get({id: $stateParams.id}).$promise;
+                            }]
+                        }
+                    }).result.then(function () {
+                        $state.go('run', null, {reload: 'run'});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            });
     }
 
 })();
