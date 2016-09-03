b076e78a89d9d4a278194c15664c043c5b5e7180
Slawek Mazur <mazursllawek@gmail.com>
2016-09-03 08:47:04 +0200

    - do some changes in register form

--- src/main/resources/static/app/account/register/register.controller.js
+++ src/main/resources/static/app/account/register/register.controller.js
@@ -25,16 +25,13 @@
         function register() {
             vm.doNotMatch = null;
             vm.error = null;
-            vm.errorUserExists = null;
             vm.errorEmailExists = null;
 
             Auth.createAccount(vm.registerAccount).then(function () {
                 vm.success = 'OK';
             }).catch(function (response) {
                 vm.success = null;
-                if (response.status === 400 && response.data === 'login already in use') {
-                    vm.errorUserExists = 'ERROR';
-                } else if (response.status === 400 && response.data === 'e-mail address already in use') {
+                if (response.status === 400 && response.data === 'e-mail address already in use') {
                     vm.errorEmailExists = 'ERROR';
                 } else {
                     vm.error = 'ERROR';
