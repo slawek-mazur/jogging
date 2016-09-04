bf8f10a2f507e084b430d636146d4e78e3c58788
Slawek Mazur <mazursllawek@gmail.com>
2016-09-04 16:53:28 +0200

    - date time picker template

--- src/main/resources/static/app/run/run.controller.js
+++ src/main/resources/static/app/run/run.controller.js
@@ -5,16 +5,23 @@
         .module('jogging')
         .controller('RunController', RunController);
 
-    RunController.$inject = ['Run', 'AlertService', 'paginationConstants', 'NgTableParams'];
+    RunController.$inject = ['Run', 'AlertService', 'pagingParams', 'paginationConstants', 'NgTableParams'];
 
-    function RunController(Run, AlertService, pagingParams, NgTableParams) {
+    function RunController(Run, AlertService, pagingParams, paginationConstants, NgTableParams) {
         var vm = this;
 
         vm.predicate = pagingParams.predicate;
         vm.reverse = pagingParams.ascending;
         vm.tableParams = new NgTableParams({}, {getData: loadAll});
+        vm.itemsPerPage = paginationConstants.itemsPerPage;
+        vm.datePickerOpenStatus = {};
+        vm.openCalendar = openCalendar;
+        vm.dateformat = dateformat;
 
         function loadAll(params) {
+
+            console.log(params.filter());
+
             return Run.query({
                 page: pagingParams.page - 1,
                 size: vm.itemsPerPage,
@@ -29,8 +36,6 @@
             }
 
             function onSuccess(data, headers) {
-                //vm.links = ParseLinks.parse(headers('link'));
-
                 params.total(headers('X-Total-Count'));
                 return data;
             }
@@ -40,6 +45,17 @@
                 params.total(0);
                 return [];
             }
+        }
+
+        vm.datePickerOpenStatus.from = false;
+        vm.datePickerOpenStatus.to = false;
+
+        function openCalendar(date) {
+            vm.datePickerOpenStatus[date] = true;
+        }
+
+        function dateformat() {
+            return 'yyyy-MM-dd';
         }
     }
 })();
